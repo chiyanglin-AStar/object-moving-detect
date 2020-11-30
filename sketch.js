f2ad4382a1325604e5b71c134ce6b04be7b8d544
chiyang <chiyanglin@gmail.com>
2020-11-30 08:22:20 +0800

    add camera switch function , but some scenario can't cover

--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@
 // p5.js Web Editor - Webcam: https://editor.p5js.org/codingtrain/sketches/VIYRpcME3
 // p5.js Web Editor - Webcam Persistence: https://editor.p5js.org/codingtrain/sketches/Vt9xeTxWJ
 
+
+//  Add switch Camera code , it is not complete by chiyang
 // let img;
 let video;
 let detector;
@@ -19,13 +21,40 @@ let switchBtn;
 
 var options = {
      video: {
-
          facingMode: {
-          exact: "environment"
+          exact: "user"
         }
      }
    };
+/*  Keep for reference
+// video constraints
+const constraints = {
+       video: {
+         width: {
+           min: 1280,
+           ideal: 1920,
+           max: 2560,
+         },
+         height: {
+           min: 720,
+           ideal: 1080,
+           max: 1440,
+         },
+       },
+     };
 
+async function initializeCamera() {
+      stopVideoStream();
+      constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+
+      try {
+           videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+           video.srcObject = videoStream;
+         } catch (err) {
+           alert("Could not access the camera");
+         }
+      }
+---------------   Keep for reference    */
 
 
 function preload() {
@@ -42,26 +71,35 @@ function gotDetections(error, results) {
 }
 
 function setup() {
-  createCanvas(640, 480);
+  ctxt = createCanvas(640, 480 + 200);
+  //initializeCamera();
+  if (  !"mediaDevices" in navigator ||
+        !"getUserMedia" in navigator.mediaDevices) {
+          alert("Camera API is not available in your browser");
+    return;
+  }else{
+      console.log(" have camera ");
+  }
+
   video = createCapture(options);
   video.size(640, 480);
   video.hide();
 
   // For Switch Camera
-  /*
-  capture = createCapture(options);
 
+  //capture = createCapture(options);
   switchBtn = createButton('Switch Camera');
   switchBtn.position(19, 19);
   switchBtn.mousePressed(switchCamera);
-  */
 
   detector.detect(video, gotDetections);
 }
 
 function draw() {
-  image(video, 0, 0);
+  image(video, 0, 200);
+
   textSize(14);
+
   text(' detect length '+ detections.length ,  20, 24);
 
   for (let i = 0; i < detections.length; i++) {
@@ -69,59 +107,64 @@ function draw() {
     let s = second();
     let millisecond = millis();
 
-    stroke(0, 255, 0);
+    stroke(0, 200+255, 0);
     strokeWeight(4);
     noFill();
-    rect(object.x, object.y, object.width, object.height);
+    rect(object.x, object.y + 200, object.width, object.height);
     noStroke();
     fill(255);
     textSize(14);
-    text('Curr sec , ms :' + s+','+ millisecond.toFixed(1) +'\n',object.x + 10, object.y -25);
-    text(' ('+ object.x.toFixed(1) +','+object.y.toFixed(1)+')',object.x + 10,object.y -10 );
+    text('Curr sec , ms :' + s+','+ millisecond.toFixed(1) +'\n',object.x + 10,200 + object.y -25);
+    text(' ('+ object.x.toFixed(1) +','+object.y.toFixed(1)+')',object.x + 10,200 + object.y -10 );
     textSize(24);
-    text(object.label, object.x + 10, object.y + 24);
+    text(object.label, object.x + 10, 200+ object.y + 24);
   }
+
 }
 
-/*
+
 function switchCamera()
 {
   switchFlag = !switchFlag;
   stopCapture();
+
   if(switchFlag==true)
   {
-   capture.remove();
-   options = {
-     video: {
-         facingMode: {
-          exact: "environment"
-        }
-     }
-   };
-
-  }
-  else
-  {
-   capture.remove();
-   options = {
+     video.remove();
+     options = {
+          video: {
+              facingMode: {
+              exact: "environment"
+              }
+            }
+          };
+     console.log("switch camera environment");
+  } else{
+    video.remove();
+    options = {
      video: {
          facingMode: {
           exact: "user"
         }
      }
    };
+   console.log("switch camera user");
   }
-  capture = createCapture(options);
+  video = createCapture(options);
+  if(!video){   //  This part should be rewrited throught " Promise "
+       console.log(" after switch camera , capture create fail ");
+  }
 }
 
 function stopCapture() {
-  let stream = capture.elt.srcObject;
-  let tracks = stream.getTracks();
-
-  tracks.forEach(function(track) {
-    track.stop();
-  });
-
-  capture.elt.srcObject = null;
+  if (this instanceof p5.MediaElement) {
+        let stream = this.elt.srcObject;
+        if (undefined != stream){
+            let tracks = stream.getTracks();
+            tracks.forEach(function(track) {
+                track.stop();
+              });
+        }
+          this.elt.srcObject = null;
+    }
 }
-*/
